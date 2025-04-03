52dc8bcfaec1b489e4afa2ebb7e0070900f93955
Kim Fransson <kim.fransson.22@gmail.com>
2025-04-03 21:32:04 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FaCalculator } from "react-icons/fa";
+
+function App() {
+  return (
+    <main className="bg-slate-100 min-h-svh grid place-items-center">
+      <h1 className="text-preset-1 text-lime flex items-center gap-100">
+        <FaCalculator /> Mortgage Calculator
+      </h1>
+    </main>
+  );
+}
+
+export default App;
